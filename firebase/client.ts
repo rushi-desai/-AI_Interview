fccc2e44678d0ac17176d2752fea0d27025d3053
Rushi <rushikeshdesai505@gmail.com>
2025-05-21 22:50:37 +0530

    env file solve

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,32 +4,19 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey:process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId:process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket:process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId:process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId:process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Initialize Firebase
-const app = !getApps.length ? initializeApp(firebaseConfig):getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig):getApp();
 
 export const auth = getAuth(app);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
 
