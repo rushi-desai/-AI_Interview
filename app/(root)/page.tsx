c5340ee49815fb132b00e5e310a3652bedc16fe1
Rushi <rushikeshdesai505@gmail.com>
2025-05-23 23:23:05 +0530

    show generated interviews on the homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,60 +1,65 @@
-import { Button } from '@/components/ui/button'
 import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { dummyInterviews } from '@/constants'
-import InterviewCard from '@/components/InterviewCard'
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import {dummyInterviews} from "@/constants";
+import InterviewCard from "@/components/InterviewCard";
+import {getCurrentUser, getInterviewsByUserId, getLatestInterviews} from "@/lib/actions/auth.action";
 
-const page = () => {
-  return (
-    <>
-    <section className='card-cta'>
-      <div className="flex flex-col gap-6 max-w-lg">
-        <h2>Interview Ready With AI-Powered Practice & Feedback</h2>
-        <p className="text-lg">
-          Practice on real interview questions & get instant feedback
-        </p>
+const Page = async () => {
+    const user = await getCurrentUser();
 
-       <Button asChild className="btn-primary max-sm:w-full">
-        <Link href="/interview">Start An Interview</Link>
-       </Button>
-   </div>
+    const [userInterviews, latestInterviews] = await Promise.all([
+        await getInterviewsByUserId(user?.id!),
+        await getLatestInterviews({ userId: user?.id! })
+    ]);
 
-   <Image src="/robot.png" alt="robo-dude" width={400} height={400} className="max-sm:hidden"/>
+    const hasPastInterviews = userInterviews?.length > 0;
+    const hasUpcomingInterviews = latestInterviews?.length > 0;
 
-    </section>
-    
-    
-    <section className="flex flex-col gap-6 mt-8">
-      <h2>Your Interviws</h2>
+    return (
+        <>
+            <section className="card-cta">
+                <div className="flex flex-col gap-6 max-w-lg">
+                    <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+                    <p className="text-lg">
+                        Practice on real interview questions & get instant feedback
+                    </p>
+                    
+                    <Button asChild className="btn-primary max-sm:w-full">
+                <Link href="/interview">Start an Interview</Link>
+                    </Button>
+                </div>
 
-      <div className="interviews-section">
-        {dummyInterviews.map((interview)=>(
-          <InterviewCard {...interview} key ={interview.id}/>
-        ))}
-      </div>
-    </section>
+                <Image src="/robot.png" alt="robo-dude" width={400} height={400} className="max-sm:hidden" />
+            </section>
 
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Your Interviews</h2>
 
-    <section className="flex flex-col gap-6 mt-8">
-   <h2>Task on Interview</h2>
-    
-    <div className="interviews-section">
+                <div className="interviews-section">
+                    {hasPastInterviews ? (
+                        userInterviews?.map((interview) => (
+                            <InterviewCard {...interview} key={interview.id}/>
+                        ))) : (
+                            <p>You haven&apos;t taken any interviews yet</p>
+                    )}
+                </div>
+            </section>
 
-    {dummyInterviews.map((interview)=>(
-          <InterviewCard {...interview} key ={interview.id}/>
-        ))}
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Take an Interview</h2>
 
-        {/* <p>You haven't taken any interviews</p> */}
-
-    </div>
-   
-
-    </section>
-
-   
-    </>
-  )
+                <div className="interviews-section">
+                    {hasUpcomingInterviews ? (
+                        latestInterviews?.map((interview) => (
+                            <InterviewCard {...interview} key={interview.id}/>
+                        ))) : (
+                        <p>There are no new interviews available</p>
+                    )}
+                </div>
+            </section>
+        </>
+    )
 }
-
-export default page
+export default Page
